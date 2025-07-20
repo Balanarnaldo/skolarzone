7a608501a3fefdebf33d468d67c094fedc7e610e
Balanarnaldo <balanbaarnaldo@gmail.com>
2025-07-20 11:02:50 -0400

    Update AdBanner.tsx

--- project/src/components/AdBanner.tsx
+++ project/src/components/AdBanner.tsx
@@ -1,1 +1,25 @@
+import React, { useEffect } from 'react';
+
+const AdBanner = () => {
+  useEffect(() => {
+    try {
+      // Initialisation manuelle
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    } catch (e) {
+      console.error('AdSense error', e);
+    }
+  }, []);
+
+  return (
+    <ins
+      className="adsbygoogle"
+      style={{ display: 'block', textAlign: 'center' }}
+      data-ad-client="ca-pub-4863368776774599"
+      data-ad-format="auto"
+      data-full-width-responsive="true"
+    ></ins>
+  );
+};
+
+export default AdBanner;
 
